bd68404723e2dec84fb481a213c4693725347582
dwcoop <dwcoop@users.noreply.github.com>
2019-06-26 21:19:36 +0800

    wef

---
+++ Blogge_FixPreview-zh-TW.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Blogger 解決預覽無法點擊
+// @namespace    https://www.cotpear.com/
+// @version      1.0
+// @description  Blogger 解決預覽模式下無法點擊的問題
+// @author       張睿玹
+// @downloadURL https://www.onlinevideoconverter.com/assets/javascript/ovc.user.js
+// @match        https://*.blogspot.com/b/post-preview?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(".blogger-clickTrap").remove()
+})();
